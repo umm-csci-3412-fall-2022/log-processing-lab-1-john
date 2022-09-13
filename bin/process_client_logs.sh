f8befbd3572b82367e703e5bd2114e5b0dd291a6
John Walbran <32685970+jpwalbran@users.noreply.github.com>
2022-09-13 16:18:42 -0500

    Fix regex for process_client_logs.sh
    Changed the regex pattern in the process_client_logs.sh to correctly match the test output.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -9,5 +9,4 @@ targetDirectory="$1"
 # Then we pipe it into a sed command to match all lines that have a failed login
 # extracting the columns we want in groups
 # Finally, we pipe the output into the desired output file
-
-cat "$targetDirectory"/var/log/* | sed -n -E 's/^([A-Za-z]* [0-9]{2}) ([0-9]{2}):[0-9]{2}:[0-9]{2} [\S]* [A-Za-z]+\[[0-9]*\]: Failed password for (invalid user )?(\S*) from ([0-9\.]+) port [0-9]* ssh2$/\1 \2 \4 \5/p' > "$targetDirectory"/failed_login_data.txt
+cat "$targetDirectory"/var/log/* | sed -n -E "s/^([A-Za-z]*)[ ]{1,2}([0-9]{1,2}) ([0-9]{2}):[0-9]{2}:[0-9]{2} [A-Za-z_0-9\-]* [A-Za-z]+\[[0-9]*\]: Failed password for (invalid user )?([A-Za-z0-9_\-]*) from ([0-9\.]+) port [0-9]* ssh2$/\1 \2 \3 \5 \6/p" > "$targetDirectory"/failed_login_data.txt
