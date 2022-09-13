b530e7f89b9315fd136b62da268c150a80ef92fa
John Walbran <32685970+jpwalbran@users.noreply.github.com>
2022-09-13 14:56:35 -0500

    Change target for sed command
    Changed the output directory from ../<file> to "$targetDirectory"/file.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -10,4 +10,4 @@ targetDirectory="$1"
 # extracting the columns we want in groups
 # Finally, we pipe the output into the desired output file
 
-cat "$targetDirectory"/var/log/* | sed -n -E 's/^([A-Za-z]* [0-9]{2}) ([0-9]{2}):[0-9]{2}:[0-9]{2} [\S]* [A-Za-z]+\[[0-9]*\]: Failed password for (invalid user )?(\S*) from ([0-9\.]+) port [0-9]* ssh2$/\1 \2 \4 \5/p' > ../failed_login_data.txt
+cat "$targetDirectory"/var/log/* | sed -n -E 's/^([A-Za-z]* [0-9]{2}) ([0-9]{2}):[0-9]{2}:[0-9]{2} [\S]* [A-Za-z]+\[[0-9]*\]: Failed password for (invalid user )?(\S*) from ([0-9\.]+) port [0-9]* ssh2$/\1 \2 \4 \5/p' > "$targetDirectory"/failed_login_data.txt
