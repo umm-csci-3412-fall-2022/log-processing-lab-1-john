c5710ed7d85e17047e4c3b82e74c2692f080bc7d
John Walbran <32685970+jpwalbran@users.noreply.github.com>
2022-09-15 14:55:07 -0500

    Add the username counting command
    Changed the original username extraction to pipe into a temporary file,
    and then added a command to sort the output, then count the number of instances with the uniq command.
    The script then prints the output of the uniq command into the (temporary) testFile.txt

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -7,4 +7,8 @@ inputDirectory="$1"
 # and get just the usernames, and dump it into a temporary file
 tempFile=$(mktemp)
 
-cat "$inputDirectory"/*/failed_login_data.txt | sed -n -E "s/^[A-Za-z]* [0-9]{1,2} [0-9]{2} ([A-Za-z0-9_-]*) .*$/\1/p" > testFile.txt
+cat "$inputDirectory"/*/failed_login_data.txt | sed -n -E "s/^[A-Za-z]* [0-9]{1,2} [0-9]{2} ([A-Za-z0-9_-]*) .*$/\1/p" > "$tempFile"
+
+# Sort the temporary file by username, then extract the counts for each username with the uniq command
+
+sort "$tempFile" | uniq -c > testFile.txt
