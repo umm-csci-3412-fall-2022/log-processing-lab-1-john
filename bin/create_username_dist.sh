765ebe0e1c9de68563fe697e70ce3af7e580b742
John Walbran <32685970+jpwalbran@users.noreply.github.com>
2022-09-15 14:46:28 -0500

    Extract username list from failed_login_data.txt
    Added the command that extracts the usernames from all the specified directory 'failed_login_data.txt' and then writes the output to a file named testFile.txt

---
+++ bin/create_username_dist.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Rename the input directory parameter to inputDirectory
+inputDirectory="$1"
+
+# Extract the contents from all instances of 'failed_login_data.txt' from the input directory
+# and get just the usernames, and dump it into a temporary file
+tempFile=$(mktemp)
+
+cat "$inputDirectory"/*/failed_login_data.txt | sed -n -E "s/^[A-Za-z]* [0-9]{1,2} [0-9]{2} ([A-Za-z0-9_-]*) .*$/\1/p" > testFile.txt
